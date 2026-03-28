c26480e2c03c371455c192246907910e9803e6e2
LucasKoc <45604254+lucaskoc@users.noreply.github.com>
2026-03-28 14:41:18 -0400

    feat: upload .wav to spectrogram

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ Hackathon IA'Hack 2026 (hackathon.uqar.ca)
 
 from config import Config
 from src.utils.dataset import AudioDataset
+from src.spectrogram.spectrogram import Spectrogram
 
 if __name__ == "__main__":
     train_dataset = AudioDataset(Config.DATASET_PATH_AUDIO + Config.PATH_TRAIN)
     test_dataset = AudioDataset(Config.DATASET_PATH_AUDIO + Config.PATH_TEST)
 
-    exit()
+    Spectrogram.wave_to_spectrogram(train_dataset, Config.PATH_TRAIN)
+    Spectrogram.wave_to_spectrogram(test_dataset, Config.PATH_TEST)
