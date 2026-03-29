1d894bc60933bd0cab4dc719a11e79bb1b86e041
LucasKoc <45604254+lucaskoc@users.noreply.github.com>
2026-03-28 20:52:12 -0400

    rect: update config

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 class Config:
+    # Files Path
     DATASET_PATH = "Data/Partie #1/Base de données/"
     PATH_TRAIN = "train/"
     PATH_TEST = "test/"
     PATH_MODEL = DATASET_PATH + "model/"
+
+    # Classes
     CLASS_NAMES = [
         "Beluga_WhiteWhale",
         "Fin_FinbackWhale",
@@ -10,7 +13,11 @@ class Config:
         "SpermWhale",
         "White_sidedDolphin",
     ]
+
+    # Audio settings
     AUDIO_SAMPLE_RATE = 22050
     AUDIO_N_MFCC = 13
-    AUDIO_DURATION = 5
+    AUDIO_DURATION = 5.0
+
+    # Classification settings
     RANDOM_STATE = 42
